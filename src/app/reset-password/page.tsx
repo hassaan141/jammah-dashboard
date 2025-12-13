34258aba98088bb1b31370bd0595a63cf9eb142b
Muhammad Farooqi <hassaanfarooqi2000@gmail.com>
2025-12-13 17:03:03 +0000

    new changes

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -113,6 +113,7 @@ export default function ResetPasswordPage() {
         </div>
       </div>
     )
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
